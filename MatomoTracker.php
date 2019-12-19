7484c538a737bf9e6c18a64dc1300befdc2ade20
sgiehl <stefan@matomo.org>
2019-12-19 15:42:57 +0100

    keep bc

--- MatomoTracker.php
+++ MatomoTracker.php
@@ -2050,3 +2050,10 @@ function Matomo_getUrlTrackGoal($idSite, $idGoal, $revenue = 0.0)
 
     return $tracker->getUrlTrackGoal($idGoal, $revenue);
 }
+
+/**
+ * For BC only
+ *
+ * @deprecated use MatomoTracker instead
+ */
+class PiwikTracker extends MatomoTracker {}
